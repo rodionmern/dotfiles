d119599c8d60ee8a1b9057898e5257d2d93733c4
rodion or radeon <114694164+rodionmern@users.noreply.github.com>
2024-01-15 13:50:01 -0500

    update install-apps.sh 1.0.3

--- files/install-apps.sh
+++ files/install-apps.sh
@@ -1,22 +1,38 @@
 
 #!/bin/bash
 
-sleep 1
-echo "Update packages:"
+sleep .1
+echo "   "
+sleep .1
+echo "---"
+sleep .1
+echo "   "
+
+
+sleep .1
+echo "Update packages - "
 sleep .5
+echo "   "
 sudo apt update
 
+echo "   "
 echo "Succesful packages update!"
+echo "   "
 
 
 sleep 1
-echo "Download packages:"
+echo "Download packages - "
 sleep .5
 sudo apt-get install gdebi gimp moc mpv vim git neofetch chromium-shell lightdm-gtk-greeter-settings telegram-desktop audacity
+
+echo "   "
+echo "---"
+echo "   "
+sleep .5
 
 echo "Succesful packages install!"
 
 
 # Copyleft (c) RodionMern
 # Version this script is
-# 1.0.2
+# 1.0.3
