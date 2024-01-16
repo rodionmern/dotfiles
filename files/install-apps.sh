d5ab9739482c20384977404fbbb32989789295ae
rodion or radeon <114694164+rodionmern@users.noreply.github.com>
2024-01-16 09:21:44 -0500

    update install-apps 1.0.4

--- files/install-apps.sh
+++ files/install-apps.sh
@@ -21,9 +21,16 @@ echo "   "
 
 
 sleep 1
+echo "Packages list: gdebi, gimp, mpv, neovim, git, neofetch, chromium-shell, lightdm-gtk-greeter-settings, telegram-desktop, audacity, blender. "
+sleep 1
+echo "   "
+sleep .1
 echo "Download packages - "
+sleep .1
+echo "   "
 sleep .5
-sudo apt-get install gdebi gimp moc mpv vim git neofetch chromium-shell lightdm-gtk-greeter-settings telegram-desktop audacity
+sudo apt-get install gdebi gimp moc mpv neovim git neofetch chromium-shell lightdm-gtk-greeter-settings telegram-desktop audacity blender
+
 
 echo "   "
 echo "---"
@@ -35,4 +42,4 @@ echo "Succesful packages install!"
 
 # Copyleft (c) RodionMern
 # Version this script is
-# 1.0.3
+# 1.0.4
