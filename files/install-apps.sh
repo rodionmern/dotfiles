882cf9edbd2b6e261767fe61ba9ce8f99763e5c0
rodion or radeon <114694164+rodionmern@users.noreply.github.com>
2024-01-17 14:03:38 -0500

    update install-apps.sh 1.0.5

--- files/install-apps.sh
+++ files/install-apps.sh
@@ -4,42 +4,58 @@
 sleep .1
 echo "   "
 sleep .1
-echo "---"
+echo "Update packages"
 sleep .1
 echo "   "
-
-
 sleep .1
-echo "Update packages - "
-sleep .5
-echo "   "
+
 sudo apt update
 
+sleep .1
 echo "   "
+sleep .1
+
 echo "Succesful packages update!"
+sleep .1
 echo "   "
-
-
-sleep 1
+sleep .1
 echo "Packages list: gdebi, gimp, mpv, neovim, git, neofetch, chromium-shell, lightdm-gtk-greeter-settings, telegram-desktop, audacity, blender. "
-sleep 1
-echo "   "
-sleep .1
-echo "Download packages - "
+
 sleep .1
 echo "   "
-sleep .5
+sleep .1
+echo "Download packages"
+sleep .1
+echo "   "
+sleep .1
+
 sudo apt-get install gdebi gimp moc mpv neovim git neofetch chromium-shell lightdm-gtk-greeter-settings telegram-desktop audacity blender
 
-
+sleep .1
 echo "   "
-echo "---"
+sleep .1
+echo "   "
+sleep .1
 echo "   "
 sleep .5
 
 echo "Succesful packages install!"
+sleep .1
+echo "   "
+sleep .1
+echo "   "
+sleep .1
+echo "   "
 
+echo "Copyleft (c) RodionMern "
+sleep .01
+echo "Version this script is  "
+sleep .01
+echo "1.0.5, you can find is  "
+sleep .01
+echo "my GitHub --            "
+sleep .01
+echo "github.com/rodionmern/  "
+sleep .01
+echo "   "
 
-# Copyleft (c) RodionMern
-# Version this script is
-# 1.0.4
