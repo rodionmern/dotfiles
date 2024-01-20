c19a6912289860b160a507bf2da2242f3a0e83d2
rodion or radeon <114694164+rodionmern@users.noreply.github.com>
2024-01-20 12:41:40 -0500

    update install-apps.sh 1.0.8, fix sleep

--- files/install-apps.sh
+++ files/install-apps.sh
@@ -2,67 +2,43 @@
 #!/bin/bash
 
 echo "||====|| ||===|| ||===||   ||===|| ||===|| ||   ||   ||       || ||===|| ||====|| ||   || " 
-sleep .1
+sleep .5
 echo "||    || ||   || ||    ||    |||   ||   || |||  ||   |||     ||| ||      ||    || |||  || " 
-sleep .1
+sleep .4
 echo "||====|| ||   || ||     ||   |||   ||   || || | ||   || |   | || ||      ||====|| || | || " 
-sleep .1
+sleep .3
 echo "||||     ||   || ||     ||   |||   ||   || ||  |||   ||  | |  || ||===|| ||||     ||  ||| " 
-sleep .1
+sleep .2
 echo "||  ||   ||   || ||    ||    |||   ||   || ||  |||   ||  |||  || ||      ||  ||   ||  ||| " 
 sleep .1
 echo "||    || ||===|| ||===||   ||===|| ||===|| ||   ||   ||   |   || ||===|| ||    || ||   || " 
 
-sleep .1
+sleep .05
 echo "   "
-sleep .1
-echo "Update packages, if you dont want update packages you can press ^C"
-sleep 1
+sleep .025
+echo "Update packages, if you dont want update packages, then you can press ^C, when starting update process."
+sleep .05
 echo "   "
-sleep .1
-echo "Update packages - 3 seconds"
-sleep 1
-echo "Update packages - 2 seconds"
-sleep 1
-echo "Update packages - 1 seconds"
-sleep 1
-echo "   "
-sleep .1
-echo "Update packages starting"
+sleep 3
 
 sudo apt update
 
-sleep .1
+sleep .05
 echo "   "
-sleep .1
-echo "   "
-sleep .1
-echo "   "
-sleep .1
 
 echo "Succesful packages update!"
 
-sleep .1
+sleep .05
 echo "   "
-sleep .1
+echo "Okey. Download packages, starts in 5 seconds."
+sleep .05
 echo "   "
-sleep .1
-echo "   "
-sleep .1
-echo "Okey. Download packages, if u dont want download packages u can press ^C"
-sleep 2
-echo "   "
-sleep .1
+sleep 5
 	
-sudo apt-get install gdebi gimp moc mpv neovim git neofetch chromium-shell lightdm-gtk-greeter-settings telegram-desktop audacity blender
+sudo apt-get install gdebi gimp moc mpv neovim git neofetch chromium lightdm-gtk-greeter-settings telegram-desktop audacity blender
 	
-sleep .1
+sleep .05
 echo "   "
-sleep .1
-echo "   "
-sleep .1
-echo "   "
-sleep .5
 
 echo "Succesful packages install!"
 
@@ -77,7 +53,7 @@ echo "Copyleft (c) RodionMern "
 sleep .01
 echo "Version this script is  "
 sleep .01
-echo "1.0.7, you can find is  "
+echo "1.0.8, you can find is  "
 sleep .01
 echo "in my GitHub profile -- "
 sleep .01
